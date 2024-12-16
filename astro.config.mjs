65efcd78d6cafc6a57d5dc6a98264fe8cd538888
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-12-16 21:30:48 +0100

    Add basic homepage (#14)
    * Update site URL in Astro configuration
    
    * Remove SVG favicon and update to ICO format in layout
    
    * Add Tailwind CSS integration and update dependencies in configuration
    
    * Update layout with personal introduction and remove unused SVG assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://laurentkempe.github.io/techheadbrothers'
+  site: 'https://new.laurentkempe.com',
+  integrations: [tailwind()]
 });
