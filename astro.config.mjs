5ff9386e816ffa2e455c34ce60f010ff59150cd9
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-12-16 18:09:26 +0100

    Update site URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://laurentkempe.github.io/techheadbrothers'
+    site: 'https://new.laurentkempe.com'
 });
