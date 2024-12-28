fe2438fcaea44d7540ce7eabd5afb80a9d77434d
Laurent Kempé <laurent.kempe@techheadbrothers.com>
2024-12-28 16:42:11 +0100

    Add vidstack (#29)
    * Rename Markdown files to MDX format and update Plyr video syntax
    
    * Add Plyr component as a VidStack and dummies Reveal & GitHubCard component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
+	theme: {},
 	plugins: [
+		require('tailwindcss-animate'),
+		require('vidstack/tailwind.cjs')({
+			prefix: 'media',
+			webComponents: true,
+		  }),
 		require('@tailwindcss/typography')
 	],
 }
